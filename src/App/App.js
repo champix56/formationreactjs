29f08af4b424aa625155b051a713c4b08e6ad1d9
jumo44 <julien.montagnier@infotel.com>
2020-10-27 15:29:34 +0100

    Hook

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Button from './components/Button/Button'
 
 function App() {
+  const maValue='124';
   return (
     <div className="App">
       <Button text="Voici le corps du bouton" backgroundColor="skyblue" color="white"/>
-      <Button text="default"/>
+      <Button text="default" onClick={()=>{console.log(maValue)}}/>
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import './Button.css'
 
 function Button(props) {
-    console.log(props);
+    //console.log(props);
+    const [clicked, setclicked] = useState();
     <Button text="Voici le corps du bouton" backgroundColor="green" color="white" />
-    return <div className="Button" style={{ backgroundColor: props.backgroundColor, color: props.color }} onClick={props.onClick}>{props.text}</div>
+    return <div className={'Button'+((clicked)?' clicked':'')} style={{ backgroundColor: props.backgroundColor, color: props.color }} onClick={
+        ()=> {
+            setclicked(true);
+            setTimeout(()=>{setclicked(false)},500)
+            props.onClick();
+        }
+    }>
+    {props.text}</div>
 }
 
 Button.propTypes = {
